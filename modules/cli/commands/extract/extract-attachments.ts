049bccc78cd9f2b076725d0d01fb29e1813639ec
Hasnae <hasnae@viqueen.org>
2022-10-08 23:34:05 +0100

    extract attachments with file extensions we know of

--- modules/cli/commands/extract/extract-attachments.ts
+++ modules/cli/commands/extract/extract-attachments.ts
@@ -4,6 +4,13 @@ import { Content } from '../../../confluence-api/types';
 import { Output } from '../../../configuration/types';
 import { api } from '../../../confluence-api';
 
+const toExtension = (mediaType: string) => {
+    const matcher = mediaType.match(/image\/(?<ext>jpeg|jpg|png)/);
+    const ext = matcher?.groups?.ext;
+    if (ext) return `.${ext}`;
+    return '';
+};
+
 export const extractAttachments = async (content: Content, output: Output) => {
     const { attachments } = content;
     if (!attachments) return;
@@ -13,10 +20,21 @@ export const extractAttachments = async (content: Content, output: Output) => {
             return api
                 .getAttachmentData(attachment.downloadUrl)
                 .then(({ stream }) => {
+                    const fileExtension = toExtension(attachment.mediaType);
                     const filePath = path.resolve(
                         output.attachments,
-                        attachment.fileId
+                        `${attachment.fileId}${fileExtension}`
                     );
+
+                    if (fileExtension !== '') {
+                        const symlink = path.resolve(
+                            output.attachments,
+                            attachment.fileId
+                        );
+                        if (!fs.existsSync(symlink))
+                            fs.symlinkSync(filePath, symlink);
+                    }
+
                     const file = fs.createWriteStream(filePath);
                     return stream.pipe(file);
                 });
