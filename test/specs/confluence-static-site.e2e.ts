623de63d73ea48ddbb0f2cffc1403732cb53c80e
Hasnae <hasnae@viqueen.org>
2022-12-19 22:24:04 +1100

    welcome visual regression test

--- test/specs/confluence-static-site.e2e.ts
+++ test/specs/confluence-static-site.e2e.ts
@@ -1,10 +1,20 @@
 describe('Confluence Static Site', () => {
     beforeEach(async () => {
+        await browser.setWindowSize(1920, 1080);
         await browser.url('http://localhost:9000/');
     });
 
     it('should render the home page', async () => {
         const title = await browser.getTitle();
         expect(title).toEqual('/conf - Git');
+    });
+
+    it('should save some screenshots', async () => {
+        await browser.saveScreen('home-page');
+    });
+
+    it('should compare successfully with a baseline', async () => {
+        const result = await browser.checkScreen('home-page');
+        expect(result).toEqual(0);
     });
 });
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,5 +1,6 @@
 import type { Options } from '@wdio/types';
 import { spawn } from 'child_process';
+import * as path from 'path';
 
 export const config: Options.Testrunner = {
     runner: 'local',
@@ -28,7 +29,21 @@ export const config: Options.Testrunner = {
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    services: ['selenium-standalone'],
+    services: [
+        'selenium-standalone',
+        [
+            'image-comparison',
+            {
+                baselineFolder: path.join(process.cwd(), 'test', 'baseline'),
+                formatImageName: '{tag}-{logName}-{width}x{height}',
+                screenshotPath: path.join(process.cwd(), '.tmp'),
+                savePerInstance: true,
+                autoSaveBaseline: true,
+                blockOutStatusBar: true,
+                blockOutToolbar: true
+            }
+        ]
+    ],
 
     framework: 'mocha',
     reporters: ['spec'],
