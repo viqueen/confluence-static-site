8e14e0529cef5a4a299fd4008692591e52784a64
Hasnae <hasnae@viqueen.org>
2022-12-19 22:51:43 +1100

    link resolver is a bit buggy with the image diff

--- test/specs/confluence-static-site.e2e.ts
+++ test/specs/confluence-static-site.e2e.ts
@@ -2,11 +2,13 @@ describe('Confluence Static Site', () => {
     beforeEach(async () => {
         await browser.setWindowSize(1920, 1080);
         await browser.url('http://localhost:9000/');
-    });
-
-    it('should render the home page', async () => {
-        const title = await browser.getTitle();
-        expect(title).toEqual('/conf - Git');
+        await browser.waitUntil(
+            async () => (await browser.getTitle()) === '/conf - Git',
+            {
+                timeout: 5000,
+                timeoutMsg: 'expected title to be different after 5s'
+            }
+        );
     });
 
     it('should save some screenshots', async () => {
